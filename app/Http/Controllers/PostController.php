141ab61e205593cf3a5891229303514f1ce8ac74
Alexx <alexx1088@gmail.com>
2022-03-06 18:37:52 +0600

    06.03

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,11 +10,41 @@ class PostController extends Controller
 
 public function index() {
 
-  $post = Posta::find(1);
+  $post = Posta::where('is_published', 1)->first();
 
   dd($post->title);
 
 }
+
+public  function create(){
+
+    $postArr = [
+        [
+        'title' => 'title of post from phpstorm',
+        'content' => 'some interesting content',
+        'image' => 'imageblabla.jpg',
+        'likes' => '20',
+        'is_published' => '1',
+        ],
+
+        [
+            'title' => 'another title of post from phpstorm',
+            'content' => 'anothersome interesting content',
+            'image' => 'another imageblabla.jpg',
+            'likes' => '50',
+            'is_published' => '1',
+        ],
+
+    ];
+
+   foreach ($postArr as $post) {
+
+     Posta::create($post);
+
+   }
+
+}
+
 
 /*
     public function update() {
